2dac673f1708df5830d1444ff46eefff116b4ca2
shakzeeofficial <shakzee171@gmail.com>
2023-09-28 18:24:59 +0500

    setup the project.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname')->nullable(true);
+            $table->text('address')->nullable(true);
+            $table->string('image')->nullable(true);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
